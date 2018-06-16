17682a0fa87a668600f7d9d080851c957cff1346
Luis Pineda JR <lspnd00@gmail.com>
2018-06-16 18:51:29 -0400

    includes tip calc and temp converter

---
+++ functions/function-scope.js
@@ -0,0 +1,22 @@
+// Global scope(convertFahrenheitToCelsius, tempOne, tempTwo)
+    // Local scope(farenheit, Celsius)
+      // Local scope(isFreezing)
+
+
+// coverting farenheit to celsius
+let convertFahrenheitToCelsius = function(farenheit){
+    let celsius = (farenheit - 32) * (5/ 9)
+
+    if(celsius <= 0){
+        let isFreezing = true
+    }
+
+
+    return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
--- functions/functions-101.js
+++ functions/functions-101.js
@@ -1,4 +1,4 @@
-// function - input(argument), code, output
+// function - input(argument), code, output(return value)
 
 let greetUser = function(){
     console.log('Welcome user!')
@@ -9,7 +9,24 @@ greetUser()
 greetUser()
 
 let square = function (num) {
-    console.log(num)
+    let result = num * num
+    return result
 }
 
-square(3)
+let value = square(3)
+let otherValue = square(10)
+
+console.log(value)
+console.log(otherValue)
+
+// coverting farenheit to celsius
+let convertFahrenheitToCelsius = function(farenheit){
+    let result = (farenheit - 32) * (5/ 9)
+    return result
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
