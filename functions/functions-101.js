6d79af02c16efedfa7cbaec182e824fa9a67b2bc
Luis Pineda JR <lspnd00@gmail.com>
2018-06-16 18:02:07 -0400

    practice makes..ok developer

---
+++ functions/functions-101.js
@@ -0,0 +1,15 @@
+// function - input(argument), code, output
+
+let greetUser = function(){
+    console.log('Welcome user!')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let square = function (num) {
+    console.log(num)
+}
+
+square(3)
