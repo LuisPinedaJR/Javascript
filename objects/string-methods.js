f5ea836752f4ffdce91b6176d34230fbb71033df
Luis Pineda JR <lspnd00@gmail.com>
2018-06-16 23:46:00 -0400

    password checker/ string

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -1,4 +1,4 @@
-let name = 'Luis Pineda'
+let name = ' Luis Pineda '
 
 // length property
 console.log(name.length)
@@ -10,6 +10,34 @@ console.log(name.toUpperCase())
 console.log(name.toLowerCase())
 
 
-// Search for or  'Includes method'
+// Search for or 'Includes method'
 let password = 'abc123password098'
-console.log(password.includes('abc'))
+console.log(password.includes('def')) // false
+console.log(password.includes('abc')) // true
+
+// Trim
+console.log(name.trim())
+
+/////////////// must be 8 in character in length and MUST NOT contain word password
+
+/// one way of cheking
+let isValidPassword = function(password){
+   if(password.length < 8 || password.includes('password')){
+        return false
+   }else{
+        return true
+    }  
+}
+console.log(isValidPassword('asdf')) // false
+console.log(isValidPassword('asdfasdfg')) // true
+console.log(isValidPassword('asdfasdfgpassword')) // false
+
+//// second way of checking
+
+let validPassword = function(password){
+    return password.length > 8 && !password.includes('password')
+}
+console.log(validPassword('asdf')) // false
+console.log(validPassword('asdfasdfg')) // true
+console.log(validPassword('asdfasdfgpassword')) // false
+
