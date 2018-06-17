a6ebcaa5a04f198764805c4a33fdc5a8a6baca25
Luis Pineda JR <lspnd00@gmail.com>
2018-06-16 22:41:28 -0400

    Expense tracker build

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -5,4 +5,21 @@ let myBook = {
 
 }
 
-console.log(myBook)
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+/////
+let person = {
+    name: 'Luis',
+    age: 30,
+    location: 'Tampa'
+}
+
+console.log(`${person.name} is ${person.age} and lives is ${person.location}.`)
+
+person.age = person.age + 1
+
+console.log(`${person.name} is ${person.age} and lives is ${person.location}.`)
