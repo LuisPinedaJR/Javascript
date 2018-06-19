79ac7c5d3fbe3e74545c6bd37cdfdd97e4573aac
Luis Pineda JR <lspnd00@gmail.com>
2018-06-18 23:58:43 -0400

    find a specific item from object array and delete it w/ splice

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,39 @@
-const notes = ['note 1', 'note 2', 'note3']
+// const notes = ['Note 1', 'Note 2', 'Note3']
+const notes = [{
+        title: 'My next trip',
+        body: 'I would like to go to spain'
+    },{
+        title: 'Habits to work on',
+        body: 'Exercise. Eating a bit better.'
+    },{
+        title: 'Office modification',
+        body: 'Get a new seat'
+    }]
+
+    const findNote = function(notes, noteTitle){
+       return notes.find(function(note, index){
+            return note.title.toLowerCase() === noteTitle.toLowerCase()
+        })
+
+    }
+
+    const note = findNote(notes, 'office modification')
+    console.log(note)
+
+
+
+    // const findNote = function(notes, noteTitle){
+    //     const index = notes.findIndex(function(note, index){
+    //         return note.title.toLowerCase() === noteTitle.toLowerCase()
+    //     })
+    //     return notes[index]
+    // }
+
+    // const note = findNote(notes, 'office modification')
+    // console.log(note)
+
+
+
 
 // notes.pop() // removes the last in array 'pops out'
 // notes.push('My new note') // adds new item in array at the end 'pushes in'
@@ -7,17 +42,13 @@ const notes = ['note 1', 'note 2', 'note3']
 
 // notes.splice(1,0, 'wtf') // this can delete and replace, just have enter the value position ex. from [1] to [1] | from [1] to [0] will add
 
-notes[2] = 'this is now the new note 3' //  replaces current item, just enter postion in array ex. [3]
+// notes[2] = 'this is now the new note 3' //  replaces current item, just enter postion in array ex. [3]
 
+// notes.forEach(function(item, index){
+//     console.log(item)
+//     console.log(index)
 
-notes.forEach(function(item, index){
-    console.log(item)
-    console.log(index)
-
-})
-
-console.log(notes.length)
-console.log(notes)
+// })
 
 //// counting ... 1
 
@@ -28,3 +59,13 @@ console.log(notes)
 // for(let count = notes.length - 1; count >= 0; count--){
 //     console.log(notes[count])
 // }
+
+// console.log(notes.length)
+// console.log(notes)
+
+// // console.log(notes.indexOf({}))
+
+// const index = notes.findIndex(function(notes, index){
+//     return notes.title === 'Habits to work on'
+// })
+// console.log(index)
--- arrays/todo.js
+++ arrays/todo.js
@@ -2,18 +2,52 @@
 // you have x todos
 // print first and second to last items -> Todo: walk the dog
 
-const todos = ['eat', 'gym,', 'laundry', 'walk the dog', 'eat']
+const todos = [{
+    text: 'eat',
+    completed: true
+}, {
+    text: 'gym',
+    completed: false
+}, {
+    text: 'Buy Food',
+    completed: true
+}, {
+    text: 'walk the dog',
+    completed: true
+}, {
+    text: 'eat again',
+    completed: false
+}]
 
-todos.splice(2,1)
-todos.push('Buy coffee')
-todos.shift()
+/// find a specifi item delete it
+const deleteTodo = function(todos, todoText){
+    const index = todos.findIndex(function (todo){
+        return todo.text.toLowerCase() === todoText.toLowerCase()
+    })
+    /// if there is a match it will 1 and if theres is no match console will return -1 so
+    // here we use index > -1 to find match
+    if(index > -1){
+        todos.splice(index, 1) /// splice is used to delete the matched item from the object above.
+    }
+}
 
-console.log(`You have ${todos.length} todos!`)
+deleteTodo(todos, 'buy food') // functioned called and arguments passed.
+console.log(todos) //logs to console results.
 
-todos.forEach(function(todo, index){ // passing a function in a function also called callback
-    const num = index + 1
-    console.log(`${num}. ${todo}`)
-})
+
+////////////////////////////////////////////
+// todos.splice(2,1)
+// todos.push('Buy coffee')
+// todos.shift()
+
+// console.log(`You have ${todos.length} todos!`)
+
+// todos.forEach(function(todo, index){ // passing a function in a function also called callback
+//     const num = index + 1
+//     console.log(`${num}. ${todo}`)
+// })
+
+////////////////////////////////////////////
 // console.log(`Todo: ${todo[0]}, ${todo[3]}`) 
 
 // for(let count = 0; count < todos.length; count++){
