6b2dbb821803f8c8db92bd42c5e7920267563c26
Luis Pineda JR <lspnd00@gmail.com>
2018-06-20 18:07:35 -0400

    using local storage parse and stringify

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -20,7 +20,26 @@ const notes = [{
 const filters = {
     searchText: ''
 }
+/////////////// LOCAL STORAGE ///////////////////////
+// const user = {
+//     name: 'Luis',
+//     age: 27
+// }
 
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
+// localStorage.setItem('location', 'Philadelphia')
+// console.log(localStorage.getItem('location'))
+// localStorage.removeItem('location')
+// localStorage.clear()
+
+///////////////////////////////////////////////////
 const renderedNotes = function(notes, filters){
     const filteredNotes = notes.filter(function(note){
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
