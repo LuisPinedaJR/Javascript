584ca1b701a159662535d8c679a9f58fb81d2dae
Luis Pineda JR <lspnd00@gmail.com>
2018-06-20 17:29:37 -0400

    filters, checkbox, forms using DOM

---
+++ notes-app/notes-app.js
@@ -0,0 +1,57 @@
+// DOM - document object model
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// Query all and remove
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to spain'
+},{
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better.'
+},{
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+
+const filters = {
+    searchText: ''
+}
+
+const renderedNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderedNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function(e){
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderedNotes(notes, filters)
+})
+
+// document.querySelector('#name-form').addEventListener('submit', function(e){
+//     e.preventDefault()
+//     console.log(e.target.elements.firstName.value)
+//     e.target.elements.firstName.value = ''
+// })
+
+// document.querySelector('#for-fun').addEventListener('change', function(e){
+//     console.log(e.target.checked)
+// })
---
+++ todo-app/todo-app.js
@@ -0,0 +1,77 @@
+const todos = [{
+    text: 'eat',
+    completed: true
+}, {
+    text: 'gym',
+    completed: false
+}, {
+    text: 'Buy Food',
+    completed: true
+}, {
+    text: 'walk the dog',
+    completed: true
+}, {
+    text: 'eat again',
+    completed: false
+}]
+
+// you have 2 todos left (p element)
+// Add a p for each todo above (use text value)
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+const renderTodos = function(todos, filters){
+    let filteredTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    filteredTodos = filteredTodos.filter(function(todo){
+        return !filters.hideCompleted || !todo.completed
+        // if(filters.hideCompleted){
+        //     return !todo.completed
+        // }else{
+        //     return true
+        // }
+    })
+
+    const incompleteTodos = filteredTodos.filter(function(todo){
+        return !todo.completed
+    })
+    
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+    
+    filteredTodos.forEach(function(todo){
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#add-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.addedTodo.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.addedTodo.value = ''
+})
+
+document.querySelector('#hide-completed').addEventListener('change', function(e){
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
