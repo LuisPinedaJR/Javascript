9ec5ed056c660c23623167957e1665e0ff10c850
Luis Pineda JR <lspnd00@gmail.com>
2018-06-26 22:50:33 -0400

    ? : if else example

---
+++ advanced/conditional-operator.js
@@ -0,0 +1,13 @@
+const myAge = 30
+let message 
+//  use '?' = yes/true/if and ':' = no/false/else
+message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
+
+//// old way of doing things if->else
+// if (myAge >= 18){
+//     message = 'You can vote!'
+// }else{
+//     message  = 'You cannot vote.'
+// }
+
+console.log(message)
