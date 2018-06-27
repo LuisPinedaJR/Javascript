6ed8e75643b9215de6394cdc46c8df0ee4de02b8
Luis Pineda JR <lspnd00@gmail.com>
2018-06-26 22:08:43 -0400

    arrow fucntions dont bind this

--- advanced/arrow-function.js
+++ advanced/arrow-function.js
@@ -16,10 +16,15 @@ const people = [{
     age: 30
 },{
     name: 'Amber',
-    age: 28
+    age: 22
 }]
 
-const under30 = people.filter(function(person){
-    return person.age < 30
-})
+// const under30 = people.filter(function(person){
+//     return person.age < 30
+// })
+
+const under30 = people.filter((person) => person.age < 30)
 console.log(under30)
+
+const age22 = people.filter((person)=> person.age <= 22)
+console.log(age22)
