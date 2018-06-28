68fa9ea6fce0eebfff23a271f462c46ef10ebbdb
Luis Pineda JR <lspnd00@gmail.com>
2018-06-27 23:31:30 -0400

    started replacing old with conditional ? :

--- advanced/conditional-operator.js
+++ advanced/conditional-operator.js
@@ -1,13 +1,28 @@
-const myAge = 30
-let message 
-//  use '?' = yes/true/if and ':' = no/false/else
-message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
+// const myAge = 30
+// //  use '?' = yes/true/if and ':' = no/false/else
+// const message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
 
-//// old way of doing things if->else
-// if (myAge >= 18){
-//     message = 'You can vote!'
-// }else{
-//     message  = 'You cannot vote.'
+// //// old way of doing things if->else
+// // if (myAge >= 18){
+// //     message = 'You can vote!'
+// // }else{
+// //     message  = 'You cannot vote.'
+// // }
+
+// console.log(message)
+
+
+// const myAge = 30
+// const showPage = () => {
+//     return 'Showing the page!'
+// }
+// const showErrorPage = () => {
+//     return 'Error, not old enough.'
 // }
 
+// const message = myAge >= 21 ? showPage() : showErrorPage()
+// console.log(message)
+
+const team = ['Tyler', 'Porter', 'Luis', 'Amber', 'Keri']
+let message = team.length <= 4 ? `Team size: ${team.length}` : 'Too many people on your team'
 console.log(message)
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -2,11 +2,7 @@
 // fetch existing todos from local storage
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
-    if(todosJSON !== null){
-        return JSON.parse(todosJSON)
-    }else{
-        return []
-    }
+    return todosJSON !== null ? JSON.parse(todosJSON) : []
 }
 // save todos to localstorage
 const saveTodos = (todos) => {
