adfc22fa9727c9aca2b3ab77177e7615016c5405
Luis Pineda JR <lspnd00@gmail.com>
2018-07-08 20:20:06 -0400

    syntax sugar constructor func and class structure

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,60 +1,58 @@
-const Hangman = function(word, remainingGuesses){
-    this.word = word.toLowerCase().split('')
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    this.status = 'playing'
-}
-
-Hangman.prototype.calculateStatus = function(){
-    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
-
-    if(this.remainingGuesses === 0){
-        this.status = 'failed'
-    }else if(finished){
-        this.status = 'finished'
-    }else{
+class Hangman {
+    constructor(word, remainingGuesses){
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
         this.status = 'playing'
     }
-}
+    calculateStatus(){
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
 
-Hangman.prototype.getStatusMessage = function(){
-    if(this.status == 'playing'){
-        return `Guesses left: ${this.remainingGuesses}`
-    }else if(this.status == 'failed'){
-        return `Nice try! the word was "${this.word.join('')}".`
-    }else{
-        return 'Great Work! You guessed the work.'
+        if(this.remainingGuesses === 0){
+            this.status = 'failed'
+        }else if(finished){
+            this.status = 'finished'
+        }else{
+            this.status = 'playing'
+        }
     }
-}
+    getStatusMessage(){
+        if(this.status == 'playing'){
+            return `Guesses left: ${this.remainingGuesses}`
+        }else if(this.status == 'failed'){
+            return `Nice try! the word was "${this.word.join('')}".`
+        }else{
+            return 'Great Work! You guessed the work.'
+        }
+    }
+    getPuzzle(word){
+        let puzzle = ''
 
-Hangman.prototype.getPuzzle = function(word){
-    let puzzle = ''
-
-    this.word.forEach((letter)=>{
+        this.word.forEach((letter)=>{
         if(this.guessedLetters.includes(letter) || letter === ' '){
             puzzle += letter
         }else{
             puzzle += '*'
         }
     })
-    return puzzle 
-}
-
-Hangman.prototype.makeGuess = function(guess){
-    guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-
-    if(this.status !== 'playing'){
-        return 
+        return puzzle 
     }
+    makeGuess(guess){
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
 
-    if(isUnique){
-        this.guessedLetters.push(guess)
-    }
-    if(isUnique && isBadGuess){
-        this.remainingGuesses--
-    }
+        if(this.status !== 'playing'){
+            return 
+        }
 
-    this.calculateStatus()
+        if(isUnique){
+            this.guessedLetters.push(guess)
+        }
+        if(isUnique && isBadGuess){
+            this.remainingGuesses--
+        }
+
+        this.calculateStatus()
+        }
 }
--- oop/person.js
+++ oop/person.js
@@ -1,32 +1,27 @@
 // prototypal inheritance 
 
-class PersonClass {
-    
-}
 
-const myPerson = new PersonClass()
-console.log(myPerson)
-
-
-const Person = function (firstName, lastName, age, likes = []){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
-Person.prototype.getBio = function(){
-    let bio = `${this.firstName} is ${this.age}.`
-    this.likes.forEach((like)=>{
+class Person {
+    constructor(firstName, lastName, age, likes = []){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio(){
+        let bio = `${this.firstName} is ${this.age}.`
+        this.likes.forEach((like)=>{
         bio += ` ${this.firstName} likes ${like}.`
     })
-    return bio
-}
 
-Person.prototype.setName = function (fullName){
-    const names = fullName.split(' ') // add space because setName needs to split at space between two names
-    this.firstName = names[0]
-    this.lastName = names[1]
-}   
+    return bio
+    }
+    setName(fullName){
+        const names = fullName.split(' ') // add space because setName needs to split at space between two names
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
+}
 
 
 const me = new Person('Luis', 'Pineda', 30, ['Learning', 'Music'])
