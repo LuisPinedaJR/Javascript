156f1abdf3975ee654c27d21f62733b9756513f2
Luis Pineda JR <lspnd00@gmail.com>
2018-07-10 20:12:03 -0400

    a synchronous js, http request

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,8 @@
+// HTTP (hypertext tranfer protocol)
+// Request - What do we want to do
+// Response - what was actually done
+
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('car parts', 2)
@@ -13,3 +18,16 @@ window.addEventListener('keypress', function (e){
     guessesEl.textContent = game1.StatusMessage
     
 })
+
+// making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange' , (e)=>{
+    if(e.target.readyState === 4){
+        data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
